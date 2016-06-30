b41c207aa37107b8442a1c24de27c1be9b72fe22
DanTheMan-2001 <danlord2001@gmail.com>
2016-06-30 14:19:36 +0100

    Login
    I have added one admin login so that you cannot add,edit or delete
    things without logging in.

--- Euro2016/Controllers/AccountController.cs
+++ Euro2016/Controllers/AccountController.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Euro2016.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
+        [HttpGet]
         public ActionResult Login()
         {
-            return View();
+            var model = new Euro2016.Models.LoginViewModel();
+            model.ReturnUrl = Request["ReturnUrl"];
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(Euro2016.Models.LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData.Add("Message", "Please enter username and password.");
+                return View(model);
+            }
+
+            if (model.Username == "DanTheMan" && model.Password == "letmein")
+            {
+                FormsAuthentication.SetAuthCookie(model.Username, false);
+                if (string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return new RedirectResult(model.ReturnUrl);
+                
+            }
+
+            TempData.Add("Message", "Invalid username and password.");
+            return View(model);
         }
     }
 }
